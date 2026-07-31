3f602601efcfd2e02591d7d1ef3c52d3e7e3cdfb
DaviesMenace1 <anonymousdavies45@gmail.com>
2026-07-31 17:10:05 +0300

    Fix import path for NewsletterSignup component

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { Separator } from '@/components/ui/separator'
-import { NewsletterSignup } from '@/components/newsletter-signup.tsx'
+import { NewsletterSignup } from '@/components/newsletter-signup'
 
 
 
@@ -30,10 +30,12 @@ const footerLinks = {
     { label: 'Support', href: '/support' },
   ],
 }
-<NewsletterSignup/> 
+
 export function SiteFooter() {
   return (
     <footer className="bg-foreground text-background">
+      <Separator className="my-6"
+       <NewsletterSignup /> 
       {/* Main footer */}
       <div className="max-w-[1440px] mx-auto px-6 lg:px-12 py-20">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-12">

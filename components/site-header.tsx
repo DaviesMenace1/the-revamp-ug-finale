a9438f6d5ef32f87c7303708a24814d67b2f435c
DaviesMenace1 <anonymousdavies45@gmail.com>
2026-08-05 08:38:20 +0300

    Add FAQs link to site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -16,6 +16,7 @@ const navLinks = [
   { label: 'Portfolio', href: '/portfolio' },
   { label: 'About', href: '/about' },
   { label: 'Journal', href: '/journal' },
+  { label: 'FAQs', href: '/faqs' },
 ]
 
 export function SiteHeader() {

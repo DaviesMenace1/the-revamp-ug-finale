a4aa74a10f4b5f12e378162a5bb876a5d6047c68
DaviesMenace1 <anonymousdavies45@gmail.com>
2026-08-11 08:25:32 +0000

    Auth Fixes

--- components/auth/custom-auth-forms.tsx
+++ components/auth/custom-auth-forms.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { FormEvent, useState } from 'react'
-import { useSignIn, useSignUp } from "@clerk/nextjs"
+import { useSignIn, useSignUp } from "@clerk/nextjs/legacy"
 import { ArrowRight, Check, Globe2, Link2, Loader2, ShieldCheck } from 'lucide-react'
 import Link from 'next/link'
 

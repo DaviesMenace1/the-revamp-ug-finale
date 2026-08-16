d2af07acbf3fa8fcaed976051768a130e300f0ce
DaviesMenace1 <anonymousdavies45@gmail.com>
2026-08-16 12:37:56 +0300

    delete drizzle/0001_add_clerk_id.sql

--- drizzle/0001_add_clerk_id.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "clerk_id" varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "brevo_contact_id" varchar(100);--> statement-breakpoint
-CREATE UNIQUE INDEX "clerk_id_idx" ON "users" USING btree ("clerk_id");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_clerk_id_unique" UNIQUE("clerk_id");
